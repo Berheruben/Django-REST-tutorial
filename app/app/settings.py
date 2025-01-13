3721f182efe47d9737224153d9f24a4998cd52b6
berheruben <rubenberhe56@gmail.com>
2025-01-13 11:38:11 +0100

    Add Docker configuration, environment variables, and proxy setup for deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i%%9y)keh(&utxb3y6(t#7)4pg6bs-%f@5yizp8i29#2+!ck(('
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS.extend(filter(None, os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")))
 
 
 
